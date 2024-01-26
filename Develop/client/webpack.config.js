15c3661abb5a3064f944014b1565acb301b5b089
Brian King <Thebeakeh@gmail.com>
2024-01-26 11:28:28 -0600

    Got webpack working and it added to the dist file

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
@@ -18,19 +18,33 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+          new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Webpack Plugin',
+         }),
+         new MiniCssExtractPlugin(),
     ],
 
     module: {
       rules: [
         {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+     }}} ]
+    }
 
 
-      
-      ],
-    },
   };
 };
